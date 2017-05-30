ca4c919046bb04c13f85ebedb61dc033cc6b166c
taibai73 <liujhao@163.com>
2017-05-30 21:14:23 +0800

    2017-5-30

--- computerManage/fileMake/tools/views.py
+++ computerManage/fileMake/tools/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, HttpResponse
 import os
 from helper.FileCls import FileCls, FileLog
 
-# Create your views here.
 def home(request):
     logDict = FileLog()
     content = {}
@@ -11,36 +10,32 @@ def home(request):
     return render(request,'home.html', content)
 
 def doMakeFiles(request):
-    sourceDir = request.POST.get('sourceDir','')
-    toDir = request.POST.get('toDir','')
-    fileListStr = request.POST.get('fileList','')
+    sourceDir = request.POST.get('sourceDir','').strip()
+    toDir = request.POST.get('toDir','').strip()
+    fileListStr = request.POST.get('fileList','').strip()
     msg = ''
     if sourceDir != '':
         if os.path.exists(sourceDir):
             pass
         else:
             msg = '源文件夹不存在！'
-            return
     else:
         msg = '请输入源文件夹路径！'
-        return
 
     if toDir != '':
         if os.path.exists(toDir):
             pass
         else:
             msg = '目标文件夹不存在！'
-            return
     else:
         msg = '请输入目标文件夹路径！'
-        return
 
     if fileListStr !='':
         fileList = fileListStr.split('\n')
         FileCls.rootPath = sourceDir
         FileCls.toDir = toDir
         FileCls.savedList = []
-        for item in fileList:
+        for i, item in enumerate(fileList):
             fpath = os.path.join(sourceDir, item[1:].strip().replace('/','\\'))
             if os.path.exists(fpath):
                 file1 = FileCls(fpath)
@@ -49,10 +44,12 @@ def doMakeFiles(request):
                 file1.saveToFile(fpath, fileCon, charset)
                 if(len(FileCls.savedList)>0):
                     msg = "<h3>成功生成以下文件：</h3>"+'<br>'.join(FileCls.savedList)
+            else:
+                if i==0:
+                    msg = "<font color=red>源文件不存在，请确认源文件路径是否正确！</font>"
 
     else:
         msg = '请输入文件列表！'
-        return
     return HttpResponse(msg)
 
 
